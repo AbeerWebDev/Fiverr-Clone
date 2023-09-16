763ac55679f76d9a452b7fa041879fb23c7fd638
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2023-09-16 21:56:59 +0400

    commit

--- fiverrui/src/components/projectCard/projectCard.jsx
+++ fiverrui/src/components/projectCard/projectCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import './projectCard'
+import './ProjectCard.scss'
+import { Link } from "react-router-dom"
 
-const projectCard = () => {
+const ProjectCard
+ = ({ item }) => {
   return (
-    <div className='projectCard'>projectCard</div>
+    <div className='projectCard'>
+      <Link className='link' to='/'>
+        <img src={item.img} alt="" />
+       <div className="info">
+        <img src={item.pp} alt="" />
+        <div className="texts">
+          <h2>{item.cat}</h2>
+          <span>{item.username}</span>
+        </div>
+       </div>
+      </Link>
+    </div>
   )
 }
 
-export default projectCard
+export default ProjectCard
